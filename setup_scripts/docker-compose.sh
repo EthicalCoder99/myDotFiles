039349f3acae773173e311922617aaab56a8ba50
LilMonk <rahulsahoo66@gmail.com>
2021-11-28 13:15:41 +0530

    Added docker and docker-compose install script

---
+++ setup_scripts/docker-compose.sh
@@ -0,0 +1,7 @@
+# https://docs.docker.com/compose/install/
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Note: If the command docker-compose fails after installation, check your path. You can also create a symbolic link to /usr/bin or any other directory in your path.
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
